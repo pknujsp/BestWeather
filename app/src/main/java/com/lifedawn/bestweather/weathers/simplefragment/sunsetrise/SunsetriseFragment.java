461ff8738af332773a53e64385eecf7ed4a33109
othean <naversovc@gmail.com>
2021-10-14 17:22:34 +0900

    sunsetrise fragment 제작 중

--- app/src/main/java/com/lifedawn/bestweather/weathers/simplefragment/sunsetrise/SunsetriseFragment.java
+++ app/src/main/java/com/lifedawn/bestweather/weathers/simplefragment/sunsetrise/SunsetriseFragment.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -14,24 +16,29 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextPaint;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.lifedawn.bestweather.R;
 import com.lifedawn.bestweather.databinding.FragmentSunsetriseBinding;
-import com.lifedawn.bestweather.weathers.dataprocessing.util.SunsetriseUtil;
 import com.lifedawn.bestweather.weathers.simplefragment.interfaces.IWeatherValues;
 import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
 import com.luckycatlabs.sunrisesunset.dto.Location;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class SunsetriseFragment extends Fragment implements IWeatherValues {
 	private FragmentSunsetriseBinding binding;
+	private Drawable sunRiseImg;
+	private Drawable sunSetImg;
+	private Location location;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -58,35 +65,47 @@ public class SunsetriseFragment extends Fragment implements IWeatherValues {
 	
 	@Override
 	public void setValuesToViews() {
-	
+		
+		
+		SunriseSunsetCalculator sunriseSunsetCalculator = new SunriseSunsetCalculator(location, TimeZone.getDefault());
+		
+		final Calendar todayCalendar = Calendar.getInstance();
+		final Calendar yesterdayCalendar = (Calendar) todayCalendar.clone();
+		final Calendar tomorrowCalendar = (Calendar) todayCalendar.clone();
+		
+		yesterdayCalendar.add(Calendar.DATE, -1);
+		tomorrowCalendar.add(Calendar.DATE, 1);
+		
+		Calendar todaySunRiseCalendar = sunriseSunsetCalculator.getOfficialSunriseCalendarForDate(todayCalendar);
+		Calendar todaySunSetCalendar = sunriseSunsetCalculator.getOfficialSunsetCalendarForDate(todayCalendar);
+		
+		if (todayCalendar.before(todaySunRiseCalendar)) {
+			//일출 전 새벽 (0시 0분 <= 현재 시각 < 일출)
+			//순서 : 전날 11.06 일몰 - 당일 11.07 일출 - 당일 11.07 일몰
+			
+		} else if (todayCalendar.compareTo(todaySunRiseCalendar) >= 0 && todayCalendar.compareTo(todaySunSetCalendar) <= 0) {
+			//일출 후, 일몰 전 (일출 <= 현재 시각 <= 일몰)
+			//순서 : 당일 11.07 일출 - 당일 11.07 일몰 - 다음날 11.08 일출
+			
+		} else {
+			//일몰 후 (일몰 < 현재 시각 < 24시 0분 )
+			//순서 : 당일 11.07 일몰 - 다음날 11.08 일출 - 다음날 11.08 일몰
+			
+		}
 	}
 	
-	static class SunSetRiseView extends View {
+	public static class TimeLineView extends View {
 		private final Paint timeLinePaint;
-		private final TextPaint sunSetRiseTypePaint;
-		private final TextPaint sunSetRiseTimePaint;
 		private final TextPaint currentTimeLabelPaint;
 		private final TextPaint currentTimeCirclePaint;
-		private final TextPaint currentTimePaint;
-		private final Drawable sunRiseImg;
-		private final Drawable sunSetImg;
-		private final Location location;
+		private final TextPaint currentTimeTextPaint;
 		
-		public SunSetRiseView(Context context, Double latitude, Double longitude) {
+		private final SimpleDateFormat dateFormat = new SimpleDateFormat("M.d E hh:mm", Locale.getDefault());
+		
+		public TimeLineView(Context context) {
 			super(context);
-			this.location = new Location(latitude, longitude);
-			
-			sunRiseImg = ContextCompat.getDrawable(context, R.drawable.temp_img);
-			sunSetImg = ContextCompat.getDrawable(context, R.drawable.temp_img);
-			
 			timeLinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 			timeLinePaint.setColor(Color.WHITE);
-			
-			sunSetRiseTypePaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-			sunSetRiseTypePaint.setColor(Color.WHITE);
-			
-			sunSetRiseTimePaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-			sunSetRiseTimePaint.setColor(Color.WHITE);
 			
 			currentTimeLabelPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 			currentTimeLabelPaint.setColor(Color.WHITE);
@@ -94,8 +113,8 @@ public class SunsetriseFragment extends Fragment implements IWeatherValues {
 			currentTimeCirclePaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 			currentTimeCirclePaint.setColor(Color.WHITE);
 			
-			currentTimePaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-			currentTimePaint.setColor(Color.WHITE);
+			currentTimeTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+			currentTimeTextPaint.setColor(Color.WHITE);
 			
 			setWillNotDraw(false);
 		}
@@ -115,35 +134,19 @@ public class SunsetriseFragment extends Fragment implements IWeatherValues {
 		protected void onDraw(Canvas canvas) {
 			super.onDraw(canvas);
 			
-			SunriseSunsetCalculator sunriseSunsetCalculator = new SunriseSunsetCalculator(location, TimeZone.getDefault());
-			
-			final Calendar todayCalendar = Calendar.getInstance();
-			final Calendar yesterdayCalendar = (Calendar) todayCalendar.clone();
-			final Calendar tomorrowCalendar = (Calendar) todayCalendar.clone();
-			
-			yesterdayCalendar.add(Calendar.DATE, -1);
-			tomorrowCalendar.add(Calendar.DATE, 1);
-			
-			Calendar todaySunRiseCalendar = sunriseSunsetCalculator.getOfficialSunriseCalendarForDate(todayCalendar);
-			Calendar todaySunSetCalendar = sunriseSunsetCalculator.getOfficialSunsetCalendarForDate(todayCalendar);
-			
-			if (todayCalendar.before(todaySunRiseCalendar)) {
-				//일출 전 새벽 (0시 0분 <= 현재 시각 < 일출)
-				//순서 : 전날 11.06 일몰 - 당일 11.07 일출 - 당일 11.07 일몰
-				
-			} else if (todayCalendar.compareTo(todaySunRiseCalendar) >= 0 && todayCalendar.compareTo(todaySunSetCalendar) <= 0) {
-				//일출 후, 일몰 전 (일출 <= 현재 시각 <= 일몰)
-				//순서 : 당일 11.07 일출 - 당일 11.07 일몰 - 다음날 11.08 일출
-				
-			} else {
-				//일몰 후 (일몰 < 현재 시각 < 24시 0분 )
-				//순서 : 당일 11.07 일몰 - 다음날 11.08 일출 - 다음날 11.08 일몰
-				
-			}
 		}
 		
 		private void drawCurrentTime(Canvas canvas) {
 		
+		}
+		
+		private Point[] drawTimeLine(Canvas canvas) {
+			Point[] linePoints = new Point[2];
+			Rect rect = new Rect();
+			String tempStr = "10.10 Mon 06:00";
+			currentTimeTextPaint.getTextBounds(tempStr, 0, tempStr.length(), rect);
+			
+			return linePoints;
 		}
 	}
 }
